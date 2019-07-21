6a644818a4daa99e7666ea6e25025c2789c6531f
AnotherTest <ali.mpfard@gmail.com>
2019-07-21 23:21:26 +0430

    a

--- install_script.sh
+++ install_script.sh
@@ -4,7 +4,7 @@ mkdir -p ~/.ipython/kernels/citron-kernel
 START_SCRIPT_PATH=$(cd `dirname "${BASH_SOURCE[0]}"` && pwd)
 CITRON_PATH=$(which ctr)
 CONTENT='{
-    "argv": ["'${CITRON_PATH}'", "'${START_SCRIPT_PATH}/citron/main.ctr'", "-quiet", "{connection_file}"],
+    "argv": ["'${CITRON_PATH}'", "'${START_SCRIPT_PATH}/citron/main.ctr'", "'${START_SCRIPT_PATH}'", "-quiet", "{connection_file}"],
     "display_name": "Citron Kernel",
     "language": "Citron"
 }'
